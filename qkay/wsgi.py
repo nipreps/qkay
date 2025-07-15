dc4577aeb907828b047e92ea804e9708665433eb
Oscar Esteban <code@oscaresteban.es>
2025-07-15 13:23:34 +0200

    fix: address deployment issues

--- qkay/wsgi.py
+++ qkay/wsgi.py
@@ -1,6 +1,7 @@
 from qkay import app
 
+
 if __name__ == "__main__":
-  app.jinja_env.auto_reload = True
-  app.config["TEMPLATES_AUTO_RELOAD"] = True
-  app.run(host="0.0.0.0")
+    app.jinja_env.auto_reload = True
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.run(host="0.0.0.0")
